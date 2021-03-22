53f09c85b75a303e5d58f78ea344250ddab237f0
gauthier poulain <gapoulai@student.42lyon.fr>
2021-03-22 12:32:12 +0100

    add .h

---
+++ libasm.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/22 12:26:29 by gapoulai          #+#    #+#             */
+/*   Updated: 2021/03/22 12:32:01 by gapoulai         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBASM_H
+# define LIBASM_H
+
+# include <stddef.h>
+# include <sys/types.h>
+
+size_t	ft_strlen(const char *s);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strdup(const char *s1);
+ssize_t	ft_read(int fd, void *buf, size_t count);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+
+#endif
