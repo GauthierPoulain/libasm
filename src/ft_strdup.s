7a3181f34d107765f32fd1e81967c288fb867f17
gauthier poulain <gapoulai@student.42lyon.fr>
2021-03-22 11:12:03 +0100

    strdup done

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -1,6 +1,21 @@
-section .text
+extern _malloc
+extern ___error
+extern _ft_strlen
+extern _ft_strcpy
 	global _ft_strdup
 
 _ft_strdup:
-	mov		rax,1
+	push	rdi
+	call 	_ft_strlen
+	inc		rax
+	mov		rdi, rax
+	call	_malloc
+	cmp		rax, byte 0
+	je		err
+	mov		rdi, rax
+	pop		rsi
+	call	_ft_strcpy
+	ret
+err
+	mov		rax, 0
 	ret
